eb48e06bf10589db2e1a169e1a4d01ee56b9c51f
Enrique Molinari <enrique.molinari@gmail.com>
2026-05-12 19:42:15 -0300

    opciones para strategy

--- src/main/java/strategy/Main.java
+++ src/main/java/strategy/Main.java
@@ -6,5 +6,15 @@ public class Main {
         var llamada2 = new Llamada(TipoLlamada.INTERNACIONAL, 12);
         System.out.println("Costo de la llamada nacional: $" + llamada1.calcularCosto());
         System.out.println("Costo de la llamada internacional: $" + llamada2.calcularCosto());
+
+//        var llamada = new Llamada(new Internacional(...));
+//        llamada.calcularCosto();
+
+//        var authenticacion = new Authenticar(new ViaGoogle(...), ... )
+//        authenticacion.proceder();
+//        var authenticacion = new Authenticar(new ViaGithub(...), ... )
+//        authenticacion.proceder();
+//        var authenticacion = new Authenticar(new UserAndPass(...), ... )
+//        authenticacion.proceder();
     }
 }
